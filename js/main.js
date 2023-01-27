cb854d897b14dfcd2a02680a2e72ebff5f89a04c
Batr <istnomad@yandex.ru>
2023-01-27 17:20:06 +0800

    27012023

--- js/main.js
+++ js/main.js
@@ -11,8 +11,28 @@ $(function () {
       '<button class="slick-arrow slick-prev"><img src="../images/slider/slide-left.svg" alt=""></button>',
     nextArrow:
       '<button class="slick-arrow slick-next"><img src="../images/slider/slide-right.svg" alt=""></button>',
-  });
+    responsive:{
+          breakpoint: 911,
+          settings: {
+            slidesToShow: 3,
+          },
+        breakpoint: 681,
+        settings: {
+          slidesToShow: 3,
+        },
+      breakpoint: 455,
+      settings: {
+        slidesToShow: 3,
+      }}
+});
+
   $(".header__menu-btn").on("click", function () {
     $(".header__menu ul").slideToggle();
   });
+
+  $(".header__drop-down.drop-down").on("click", function () {
+    $(this).children(".drop-down__list").slideToggle();
+    $(this).children("a").slideToggle();
+  });
+
 });
